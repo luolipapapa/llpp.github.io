0c72785fbc9d9521c8dc445db363b9e2333809f0
luolipapapa <44192263+luolipapapa@users.noreply.github.com>
2018-12-08 10:47:54 -0800

    Create ad.js

---
+++ ad.js
@@ -0,0 +1,15 @@
+ $(function(){
+   layer.open({
+     type: 1,
+     area: '255',
+     title:null,
+     shadeClose: true, 
+     content: '<a href="http://t.cn/EyuqN6F" target="_blank"><img width="200" height="200" src="https://www.luolipapa.ml/05.gif" /></a>' //è¿™é‡Œæ˜¯ä¿®æ”¹å›¾ç‰‡åœ°å€å’Œå›¾ç‰‡é“¾æŽ¥çš„
+   });
+   var top = ($(window).height() - 200) / 2;
+   var left = $(window).width() / 2 - 200/ 2;
+   layer.style(1, {
+     top: top,
+     left:left
+   });
+});
