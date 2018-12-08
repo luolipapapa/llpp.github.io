002c98990fd422759e73059727c263047f72a3c4
luolipapapa <44192263+luolipapapa@users.noreply.github.com>
2018-12-08 11:05:58 -0800

    Update ad.js

--- ad.js
+++ ad.js
@@ -6,8 +6,8 @@
      shadeClose: true, 
      content: '<a href="http://t.cn/EyuqN6F" target="_blank"><img width="200" height="200" src="https://www.luolipapa.ml/05.gif" /></a>' //è¿™é‡Œæ˜¯ä¿®æ”¹å›¾ç‰‡åœ°å€å’Œå›¾ç‰‡é“¾æŽ¥çš„
    });
-   var top = ($(window).height() - 200) / 2;
-   var left = $(window).width() / 2 - 200/ 2;
+   var top = ($(window).height() - 180) / 2;
+   var left = $(window).width() / 2 - 180/ 2;
    layer.style(1, {
      top: top,
      left:left
